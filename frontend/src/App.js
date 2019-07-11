4e9ad82bbb22b7a99db870663bd25b2c7bfb184b
Ben Q <Quackenbush117@gmail.com>
2019-07-10 21:30:27 -0600

    2nd commmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,75 +1,22 @@
 import React, {Component} from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
 
-import './App.css';
+import Navigation from './components/functional/Navigation'
+import './App.scss';
 
 class App extends Component {
   constructor() {
     super();
-    this.toggle = this.toggle.bind(this);
     this.state = {
       bucketList: []
     }
   }
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      })
-    }
-  render() {
-
-    
-
+render() {
     return (
       <div className="App">
-                <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">BucketList</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <Navigation />
       </div>
     );
   }  
-
-  }
-  
-
+}
 
 export default App;
