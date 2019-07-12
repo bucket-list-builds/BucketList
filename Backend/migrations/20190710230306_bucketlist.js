5bd2a29acf4d2b8d10de79625ed285cc119086b8
Max Keller <max.keller427@gmail.com>
2019-07-11 21:16:06 -0600

    added and ran migrations;

--- Backend/migrations/20190710230306_bucketlist.js
+++ Backend/migrations/20190710230306_bucketlist.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("bucketlist", bl => {
+    bl.increments();
+
+    bl.string("title").notNullable();
+    bl.string("description").notNullable();
+
+    bl.integer("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+      .notNullable();
+
+    bl.integer("created_at").notNullable();
+    bl.string("photo");
+    bl.string("journal_entry");
+    bl.boolean("completed").notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("bucketlist");
 };
