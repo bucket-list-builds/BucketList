442c95da6cd40975f3018ebfe298a13ad7f7b7f8
Reyaad Rafeek <reyaad510@gmail.com>
2019-07-12 17:04:59 -0400

    Added code into restricted-middleware but not implemented yet, added /register to add new users to db with no authentication implemented until needed

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router.js');
 const bucketlistRouter = require('../bucketlist/bucketlist-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -11,6 +12,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/auth', authRouter);
 server.use('/home', bucketlistRouter);
 server.use('/users', usersRouter);
 
