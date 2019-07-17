beadbefc50f9402361dd1290ffc6dfd8a9fac111
Ben Q <Quackenbush117@gmail.com>
2019-07-16 19:23:31 -0600

    Registration is working. Able to log state to console

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import Navigation from './components/functional/Navigation'
 import './App.scss';
 import LoginPage from './components/view/loginPage';
+import RegistrationPage from './components/view/registrationPage'
 
 class App extends Component {
   constructor() {
@@ -16,6 +17,7 @@ render() {
       <div className="App">
         <Navigation />
         <LoginPage />
+        <RegistrationPage />
       </div>
     );
   }  
