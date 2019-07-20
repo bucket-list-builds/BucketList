45a70933ff756ccb5dd2689101fb2043092175e5
Ben Q <Quackenbush117@gmail.com>
2019-07-19 19:51:00 -0600

    User ID is now in local storage after login.

--- frontend/src/components/view/loginPage.js
+++ frontend/src/components/view/loginPage.js
@@ -6,7 +6,7 @@ class LoginPage extends Component {
        this.state = {
            username: '',
            password: '',
-           // error: ''
+           
        }
 
        this.handlePassChange = this.handlePassChange.bind(this);
@@ -19,24 +19,12 @@ class LoginPage extends Component {
        this.setState({ error: ''});
    }
 
-   // handleSubmit(event) {
-   //     event.preventDefault();
-
-   //     if (!this.state.username) {
-   //         return this.setState({ error: 'Username is required'});
-   //     }
-
-   //     if (!this.state.password) {
-   //         return this.setState({ error: 'Password is required'});
-   //     }
-   //     return this.setState({ error: 'Fake Log In Success'});
-   // }
-
    handleSubmit = event => {
        event.preventDefault();
        axios.post('https://bucketlist-builds.herokuapp.com/api/auth/login', this.state)
          .then(res => {
            localStorage.setItem('jwt', res.data.token);
+           localStorage.setItem('user_id', res.data.user_id)
            this.props.history.push('/home');
          }).catch(err => {
            console.log(err);
@@ -63,13 +51,7 @@ class LoginPage extends Component {
        return (
            <div className="Login">
                <form onSubmit={this.handleSubmit}>
-                   {/* {
-                       this.state.error &&
-                       <h3 data-test="error" onClick={this.dismissError}>
-                           <button onClick={this.dismissError}>X</button>
-                           {this.state.error}
-                       </h3>
-                   } */}
+                  
                    <label>User Name</label>
                    <input
                        type="text"
